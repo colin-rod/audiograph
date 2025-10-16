096631db0403192f14cad6e5fa5889d6a2b10481
colin-rod <140830677+colin-rod@users.noreply.github.com>
2025-10-16 21:41:33 +0200

    Fix instrumentation to lazy-load server Sentry handlers

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,7 +1,9 @@
-import { registerServerExceptionHandlers } from './src/lib/monitoring/sentry/server'
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const { registerServerExceptionHandlers } = await import(
+      './src/lib/monitoring/sentry/server'
+    )
+
     registerServerExceptionHandlers()
   }
 }
